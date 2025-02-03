558508e770c1bd60e998ebd6f9448511006375aa
Pedro-Luis-Informatica <valenzuelapedroluis338@gmail.com>
2025-02-03 09:07:00 -0400

    variables y tipos

---
+++ app.js
@@ -0,0 +1,32 @@
+// Variables y Tipos de Datos
+var nombre = "Pedro"; // String
+let cedula = "123456789"; // String
+let edad = 25; // Number
+let sueldo = 2500.0; // Number
+let hijos = false; // Boolean
+
+const PI = 3.1416; // constante, no cambiará su valor -- PI = 3.14 -- Error, no se puede cambiar el valor de una const
+
+/*
+String (texto)	"Hola, mundo"
+Number (número)	42, 3.14
+Boolean (booleano)	true, false
+Array (arreglo)	[1, 2, 3, 4]
+Object (objeto)	{ nombre: "Pedro", edad: 25 }
+*/
+
+// Operadores Aritméticos
+let suma = 7 + 8;
+let resta = 20 - 4;
+let multiplicacion = 9 * 1;
+let division = 25 / 5;
+let modulo = 30 % 3;
+let exponente = 5 ** 2;
+
+console.log(suma, resta, multiplicacion, division, modulo, exponente);
+
+let saludo = "hola ";
+let persona = prompt("ingresa tu nombre: ");
+
+console.log(saludo, persona);
+alert("mañana hay examen de bases de datos");
