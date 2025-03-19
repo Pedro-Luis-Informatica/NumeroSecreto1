f366c7cbf9a358d601c2b80ee658563c2b19a8ed
Pedro-Luis-Informatica <valenzuelapedroluis338@gmail.com>
2025-03-19 12:54:02 -0400

    eventos

--- app.js
+++ app.js
@@ -1,6 +1,75 @@
-let valor = document.getElementById("valor"); //selector de id
-let valor2 = document.querySelector(".valorClase"); //selector de clase
-let h2 = document.querySelector("h2"); //selector de etiqueta
+//metodos de seleccion de elementos del DOM
+//getElementById
+//GetElementsByClassName
+//getElementsByTagName
+//querySelector
+//querySelectorAll
+//cloneNode(deep)
 
-valor.innerHTML = "Cacique"; // Cambia el texto del elemento
-h2.innerHTML = "Indio poner punto rojo"; // Cambia el texto del elemento
+//metodos de manipulacion de elementos del DOM
+//contenido y texto
+/*
+innerHTML
+element.textContent
+element.innerText
+*/
+//atributos
+/*
+setAttribute
+getAttribute
+removeAttribute
+*/
+//clases
+/*
+element.classList.add(nombreClase)
+element.classList.remove(nombreClase)
+element.classList.toggle(nombreClase)
+element.classList.contains(nombreClase)
+ */
+//estilos
+/*
+element.style.propiedadCSS
+element.style.setProperty(propiedad, valor, prioridad)
+element.style.removeProperty(propiedad)
+window.getComputedStyle(elemento).propiedadCSS
+ */
+//creacion y eliminacion de elementos
+/*
+document.createElement(nombreElemento)
+document.createTextNode(texto)
+parent.appendchild(element)
+parent.removechild(childNode)
+parent.lastchild // accede al ultimo nodo
+parent.replacechild(newchild, oldchild)
+parent.insertbefore(new element, reference element)
+*/
+
+// propiedades de los nodos
+//nodeType
+//node.parentNode
+//element.childNode
+//nodeName
+//nodeValue
+//classList
+//style
+//outerHTML
+//node.firstChild
+//node.lastChild
+//node.nextChild
+//node.cloneNode(deep)
+//node.previusSibling
+//node.nextElementSibling
+
+let btnSaludar = document.querySelector('.btn-saludar');
+let h1 = document.querySelector('h1')
+//let nombre = prompt("Introduce tu nombre: ")
+
+btnSaludar.addEventListener('mouseover', function(){ // cuando pase poer el boton
+    h1.style.backgroundColor = "yellow";
+});
+
+btnSaludar.addEventListener('mouseout', function(){ //cuando salga del boton
+    h1.style.backgroundColor = 'red';
+})
+ 
+//eventos con el addEventListener, funcionalidad y ejemplos.
